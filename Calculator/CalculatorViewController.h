e90d76226cd3303516e8e2e42b1d43279636544f
Iain Smith <b099l3@macbook.home>
2011-11-20 19:00:03 +0000

    Initial Commit

---
+++ Calculator/CalculatorViewController.h
@@ -0,0 +1,13 @@
+//
+//  CalculatorViewController.h
+//  Calculator
+//
+//  Created by Iain Smith on 20/11/2011.
+//  Copyright (c) 2011 b099l3. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CalculatorViewController : UIViewController
+
+@end
---
+++ Calculator/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Calculator
+//
+//  Created by Iain Smith on 20/11/2011.
+//  Copyright (c) 2011 b099l3. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CalculatorAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CalculatorAppDelegate class]));
+    }
+}
